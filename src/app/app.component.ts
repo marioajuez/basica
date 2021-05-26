09af0bb60254b859fe9849e01cffa8cb03a264f0
marioajuez <marioajuez@gmail.com>
2021-05-26 11:59:49 -0500

    table with dates incremeting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hyperfund-calc';
+  today;
+  totalDays = 10;
+  table = []
+  constructor(){
+    for( let i = 1; i<=this.totalDays;i++){
+        this.table.push({
+            date: new Date().setDate(new Date().getDate()+i)
+        })
+    }
+  }
 }
