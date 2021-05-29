544e5bc3a538313f521769ed5cbdc760694a7b94
marioajuez <marioajuez@gmail.com>
2021-05-28 21:45:42 -0500

    tabla con todos los calculos 100% falta agregarle los eventos de cambio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 
-import {FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 
 const ELEMENT_DATA: any[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
 @Component({
@@ -24,61 +25,78 @@ const ELEMENT_DATA: any[] = [
 export class AppComponent {
 
   memberShip: any[] = [
-    {value: '300', viewValue: 'HU 300'},
-    {value: '500', viewValue: 'HU 500'},
-    {value: '1000', viewValue: 'HU 1000'}
+    { value: '300', viewValue: 'HU 300' },
+    { value: '500', viewValue: 'HU 500' },
+    { value: '1000', viewValue: 'HU 1000' }
   ];
 
-  selectedMembership = this.memberShip[0].value;
-
-  campaignOne: FormGroup;
-  campaignTwo: FormGroup;
-
-  date = new FormControl(new Date());
-  date_ = new Date();
+  userData = {
+    date: new Date(),
+    membership: this.memberShip[0].value,
+  };
 
   table = []
-  displayedColumns: string[] = ['day', 'date', 'dailyRewards','rebuy' ,'balance'];
-  dataSource = this.table
-  today;
+  displayedColumns: string[] = ['day', 'date', 'amount', 'dailyInterest', 'dailyRewards', 'rebuy', 'balance'];
   totalDays = 600;
- 
-  constructor(){
 
-    const today = new Date();
-    const month = today.getMonth();
-    const year = today.getFullYear();
+  constructor() {
 
-    this.campaignOne = new FormGroup({
-      start: new FormControl(new Date(year, month, 13)),
-      end: new FormControl(new Date(year, month, 16))
-    });
+    let dailyInterest = 0
+    let dailyRewards = 0
+    let amount = parseInt(this.userData.membership)
 
-    this.campaignTwo = new FormGroup({
-      start: new FormControl(new Date(year, month, 15)),
-      end: new FormControl(new Date(year, month, 19))
-    }); 
+    // let membershipBalance = 900
 
-    let aux=0
-    for( let i = 1; i<=this.totalDays;i++){
+    // let aux = 900
 
-        aux+=this.memberShip[0].value*(0.005)
-        this.table.push({
-            date: new Date().setDate(new Date().getDate()+i),
-            dailyRewards: aux
-        })
+    let rebuy = 0;
+    let membershipBalance = 900 - 1.50
+
+    let aux = 900 -1.50
+
+    for (let i = 1; i <= this.totalDays; i++) {
+
+      dailyInterest = amount * (0.005)
+      dailyRewards += amount * (0.005)
+
+      if (dailyRewards >= 50)
+          rebuy = 50
+      else {
+          rebuy = 0
+      }
+
+      this.table.push({
+        date: new Date().setDate(new Date().getDate() + i),
+        amount: amount,
+        dailyInterest: dailyInterest,
+        dailyRewards: dailyRewards,
+        rebuy: rebuy,
+        membershipBalance: membershipBalance
+      })
+
+      if (dailyRewards >= 50) {
+        amount +=50
+        dailyRewards -= 50
+        membershipBalance+=(rebuy*3)-(amount * (0.005));
+      }
+      else{
+        membershipBalance+=(rebuy*3)-(amount * (0.005));
+      }
     }
   }
 
-  addEvent(type: string, event) {
+
+  addEvent() {
 
     this.table = []
+    for (let i = 1; i <= this.totalDays; i++) {
+      this.table.push({
+        date: new Date(this.userData.date).setDate(new Date(this.userData.date).getDate() + i),
+        dailyRewards: 0
+      })
+    }
+  }
+  select() {
 
-//     for( let i = 1; i<=this.totalDays;i++){
-//       this.table.push({
-//           date: new Date(event.value).setDate(new Date(event.value).getDate()+i),
-//           dailyRewards: "hola"
-//       })
-//   }
-}
+  }
 }
