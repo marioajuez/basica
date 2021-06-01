c8bd1e99623978f7891f050144025e2d45ff0595
marioajuez <marioajuez@gmail.com>
2021-06-01 01:47:08 -0500

    se encuentra funcionando la funcionalidad de reinvertir dinamicamente (rebuy)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,18 @@
+import { CdkVirtualForOf } from '@angular/cdk/scrolling';
 import { Component } from '@angular/core';
 
 import { FormGroup, FormControl } from '@angular/forms';
 
-const ELEMENT_DATA: any[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
 
+export interface Table{
+    date,
+    amount,
+    dailyInterest,
+    dailyRewards,
+    rebuy,
+    membershipBalance,
+
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -28,12 +26,17 @@ export class AppComponent {
     { value: '10000', viewValue: 'HU 10.000'},
   ];
 
+  
+  
   userData = {
     date: new Date(),
-    membership: this.memberShip[0].value,
+    membership: "300"
   };
+  
+   table: any[] = [];
 
-  table = [];
+   timeout: any = null;
+
   displayedColumns: string[] = [
     'day',
     'date',
@@ -41,23 +44,27 @@ export class AppComponent {
     'dailyInterest',
     'dailyRewards',
     'rebuy',
+    'optionRebuy',
     'balance',
   ];
   totalDays = 600;
+  // rebuy=0
+  // dailyRewards= 0  nan
+  // amount;
 
   constructor() {
-    this.createTable();
+
+    this.initilizateTable();
   }
 
-  select() {
-    this.createTable();
-  }
-  dataPicker(event?) {
-    this.createTable();
+  public triggerEventKey(event: any) {
+    clearTimeout(this.timeout);
+    this.timeout= setTimeout( () => {
+         this.createTable();
+    }, 500);
   }
 
-  createTable(){
-    this.table = [];
+  initilizateTable(){
     let dailyRewards = 0;
     let amount = parseInt(this.userData.membership);
 
@@ -79,6 +86,7 @@ export class AppComponent {
         dailyRewards: dailyRewards,
         rebuy: rebuy,
         membershipBalance: membershipBalance,
+        isCheck: true
       });
 
       if (dailyRewards >= 50) {
@@ -88,4 +96,83 @@ export class AppComponent {
       } else membershipBalance += rebuy * 3 - amount * 0.005;
     }
   }
+
+
+  public check(event, indice){
+
+    let indice_ = indice+1
+
+    this.table[indice_].isCheck = event;
+
+    let rebuy = this.table[indice].rebuy;
+    let amount= (this.table[indice].amount+ rebuy) - (rebuy);
+    let dailyRewards = (this.table[indice].dailyRewards - rebuy)+amount*0.005;
+    let membershipBalance = this.table[indice].membershipBalance - amount*0.005 +(3*rebuy)
+
+
+    for( let i = indice_; i < this.table.length;i++){
+
+      rebuy = parseFloat(((dailyRewards/50.00).toString()).split(".")[0])*50.00
+
+
+      this.table[i].amount= amount;
+      this.table[i].dailyInterest= amount * 0.005;
+      this.table[i].dailyRewards= dailyRewards
+      this.table[i].rebuy = rebuy
+      this.table[i].membershipBalance = membershipBalance
+
+
+      if(dailyRewards >= 50) {
+        dailyRewards -= rebuy;
+        amount +=rebuy
+        membershipBalance += rebuy * 3 - amount * 0.005;
+      } 
+      else membershipBalance += rebuy * 3 - amount * 0.005;
+
+      dailyRewards += amount * 0.005;
+
+    }
+
+  }
+
+
+  select() {
+    // this.createTable();
+  }
+  dataPicker(event?) {
+    // this.createTable();
+  }
+
+  createTable(){
+
+
+          let dailyRewards = 0;
+          let amount = parseFloat(this.userData.membership);
+          if(300>amount) amount=300
+          let rebuy = 0;
+          let membershipBalance = (amount*3) - (amount*0.005);
+
+          this.table.forEach( (element,index) => {
+              dailyRewards += amount * 0.005;
+
+              rebuy = parseFloat(((dailyRewards/50.00).toString()).split(".")[0])*50.00
+
+              element.date= new Date(this.userData.date).setDate(new Date(this.userData.date).getDate() + (index+1))
+              element.amount= amount
+              element.dailyInterest= amount * 0.005,
+              element.dailyRewards= dailyRewards
+              element.rebuy= rebuy
+              element.membershipBalance= membershipBalance
+
+
+              if (dailyRewards >= 50) {
+                amount+= rebuy;
+                dailyRewards -= rebuy;
+                membershipBalance += rebuy * 3 - amount * 0.005;
+              } 
+              else membershipBalance += rebuy * 3 - amount * 0.005;
+        })
+  
+
+}
 }
